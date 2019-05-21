44ba7b089a57bfd38650711fa5bbbd02ce43e23c
Castro0o <andre@andrecastro.info>
2019-05-21 09:19:49 +0200

    make.py is using a general function to loop+convert files

--- make.py
+++ make.py
@@ -15,9 +15,9 @@ parser = ArgumentParser(prog=sys.argv[0],
                         description="Script to convert \
                         docx files to other format: icml, html, website")
 parser.add_argument("output",
-                    choices=['website', 'html', 'icml'],
+                    choices=['website', 'html', 'icml', 'pdf'],
                     default='website',
-                    help="output formats: website, html, icml")
+                    help="output formats: website, html, icml, pdf")
 
 args = parser.parse_args()
 
@@ -29,55 +29,50 @@ metadata = readyaml(dir_parent + '/' + 'publication_metadata.yaml')
 metadata = TOC_populate(dir_parent, metadata)  # add full path dirs to TOC
 pprint(metadata)
 
+
+def convert_loop(TOC, _from, _to):
+    '''
+    Loop through all files TOC
+    Converting them from _from to _to format
+    '''
+    for text_entry in TOC:
+        print("{} >> {}".format(text_entry[_from],
+              text_entry[_to]), "\n")
+        text_entry_content = pandoc_convert(inputfile=text_entry['docx'],
+                                            _from=_from,
+                                            _to=_to)
+        with open(text_entry[_to], 'w') as outfile:
+            outfile.write(text_entry_content)
+
+
 if args.output == 'website':
     print('Making {}'.format(args.output))
     env = jinja_env(dir_parent + '/website-templates')
     # create website's text_entries
+    convert_loop(TOC=metadata['TOC'],
+                 _from='docx',
+                 _to='html')
     for text_entry in metadata['TOC']:
-        # pprint(text_entry)
-        print("{} >> {}".format(text_entry['docx'], text_entry['html']), "\n")
-
-        text_entry_content = pandoc_convert(inputfile=text_entry['docx'],
-                                            _from='docx',
-                                            _to='html')
+        print(text_entry['html'])
+        with open(text_entry['html'], 'r') as html_file:
+            html_text = html_file.read()
+            # print(html_text)
         webpage = jinja_render_template(
             env=env,
             tmpl_file='contentpage.html',
             title=text_entry['title'],
             TOC=metadata['TOC'],  # used in menu
-            content=text_entry_content
+            content=html_text
             # TODO: add authors
         )
         filename = (text_entry['html'].replace('/html/', '/website/'))
         with open(filename, 'w') as webpagefile:
             webpagefile.write(webpage)
-        # print(webpage)
-    # create index
-    webpage_idex = jinja_render_template(
-        env=env,
-        tmpl_file='contentpage.html',
-        title=metadata['Title'],
-        TOC=metadata['TOC'],  # used in menu
-        content=''
-        # TODO: add authors
-    )
 
 elif args.output == 'html':
-    print('Making {}'.format(args.output))
-    for text_entry in metadata['TOC']:
-        print("{} >> {}".format(text_entry['docx'], text_entry['html']), "\n")
-        text_entry_content = pandoc_convert(inputfile=text_entry['docx'],
-                                            _from='docx',
-                                            _to='html')
-        with open(text_entry['html'], 'w') as htmlfile:
-            htmlfile.write(text_entry_content)
-
+    convert_loop(TOC=metadata['TOC'], _from='docx', _to='html')
 elif args.output == 'icml':
-    print('Making {}'.format(args.output))
-    for text_entry in metadata['TOC']:
-        print("{} >> {}".format(text_entry['docx'], text_entry['icml']), "\n")
-        text_entry_content = pandoc_convert(inputfile=text_entry['docx'],
-                                            _from='docx',
-                                            _to='icml')
-        with open(text_entry['icml'], 'w') as htmlfile:
-            htmlfile.write(text_entry_content)
+    convert_loop(TOC=metadata['TOC'], _from='docx', _to='icml')
+elif args.output == 'pdf':
+    convert_loop(TOC=metadata['TOC'], _from='docx', _to='html')
+    # ICML STUFF
